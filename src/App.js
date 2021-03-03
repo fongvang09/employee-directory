956af69b6d46c603a4ce32a552404fa6ba48645b
fongvang09 <koichi_vang@hotmail.com>
2021-03-03 16:20:56 -0600

    test

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from "react";
+
+// const url = "https://randomuser.me/api/?results=200&nat=us";
+// const url = "https://randomuser.me/";
+
+function App() {
+  return (
+    <>
+
+    </>
+    );
+}
+
+export default App;
